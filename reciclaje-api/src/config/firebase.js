d9b8708804a9c910dee4a914ab249b515c0b2a9b
euwnatalia <euwnatalia@gmail.com>
2025-09-28 20:53:43 -0300

    Eliminación de credenciales de firebase.js

--- reciclaje-api/src/config/firebase.js
+++ reciclaje-api/src/config/firebase.js
@@ -1,13 +1,17 @@
 const admin = require('firebase-admin');
 require('dotenv').config();
 
-const serviceAccount = require('./firebase-key.json');
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+};
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount)
 });
 
-const db   = admin.firestore();
+const db = admin.firestore();
 const auth = admin.auth();
 
 module.exports = { db, auth };
